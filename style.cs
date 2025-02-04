43c6b67f8b303fabdab97945fb80a781ceef1134
shotabakh <bkhbakhashvilishota@gmail.com>
2025-02-04 22:37:16 +0400

    one

---
+++ style.cs
@@ -0,0 +1,13 @@
+html {
+    background-color: rgb(255, 0, 0);
+}
+
+body {
+    background-color: rgba(9, 255, 0, 0.377);
+    padding:  50px;
+    margin:  40 80;
+}
+.full-width-image {
+    width: 100%;
+    height: auto;
+}
